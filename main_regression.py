7f789868dae2ed8db5f26460fd72961209aad823
yldas <misael.valentin@upr.edu>
2020-09-26 17:57:13 -0400

    Added regression and regression stats

--- main_regression.py
+++ main_regression.py
@@ -21,19 +21,19 @@ for fold, (train, test) in enumerate(skf.split(X=np.zeros(len(df)), y=df.iloc[:,
 # Train; run 5 experiments in total
 trained_models = []
 for training_set in training_sets:
-    my_knn = KNN(num_neighbors=3, bandwidth=1, error=2.0, data=training_set, attribute_names=list(df.columns))
+    my_knn = KNN(num_neighbors=3, bandwidth=1, error=5.0, data=training_set, attribute_names=list(df.columns))
     my_knn.train(regression=True)
     my_knn.report_training_stats()
     trained_models.append(my_knn)
 
 # Tune parameters for 5 trained models
-# for model in trained_models:
-#     parameters = [_ for _ in range(1, 11)]
-#     optimal_parameter = model.tune(tuning_set, parameters=parameters)
-#     model.report_tuning_stats()
-#     print("Optimal parameter: ", optimal_parameter)
-#
-# # Test
-# for model, test_set in zip(trained_models, test_sets):
-#     model.test(test_set)
-#     model.report_testing_stats()
+for model in trained_models:
+    parameters = [_ for _ in range(1, 11)]
+    optimal_parameter = model.tune(tuning_set, parameters=parameters, regression=True)
+    model.report_tuning_stats()
+    print("Optimal parameter: ", optimal_parameter)
+
+# Test
+for model, test_set in zip(trained_models, test_sets):
+    model.test(test_set, regression=True)
+    model.report_testing_stats()
--- models/KNN_pd.py
+++ models/KNN_pd.py
@@ -38,11 +38,16 @@ class KNN:
             self.data.apply(self.classify, axis=1)
         else:
             self.data.apply(self.regress, axis=1)
+            self.training_stats['mean_squared_error'] /= self.training_stats['total_predictions']
 
-    def test(self, test_set):
-        test_set.apply(self.classify, testing=True, axis=1)
+    def test(self, test_set, regression=False):
+        if not regression:
+            test_set.apply(self.classify, testing=True, axis=1)
+        else:
+            self.data.apply(self.regress, testing=True, axis=1)
+            self.test_stats['mean_squared_error'] /= self.test_stats['total_predictions']
 
-    def tune(self, tuning_set, parameters):
+    def tune(self, tuning_set, parameters, regression=False):
         for parameter in parameters:
             self.num_neighbors = parameter
             self.parameter_tuning[parameter] = {
@@ -55,7 +60,12 @@ class KNN:
                 'false_positive': 0,
                 'false_negative': 0,
             }
-            tuning_set.apply(self.classify, parameter=parameter, tuning=True, axis=1)
+            if not regression:
+                tuning_set.apply(self.classify, parameter=parameter, tuning=True, axis=1)
+            else:
+                tuning_set.apply(self.regress, parameter=parameter, tuning=True, axis=1)
+                self.parameter_tuning[parameter]['mean_squared_error'] /= \
+                    self.parameter_tuning[parameter]['total_predictions']
 
         optimal_parameter, max_success_rate = 1, 0
         for k, v in self.parameter_tuning.items():
@@ -107,17 +117,26 @@ class KNN:
         nearest_values = nearest_neighbors['Class']
         weighted_sum, normalizer = 0, 0
         for distance, value in zip(nearest_distances, nearest_values):
-            weighted_sum += np.exp(-(1/2*self.bandwidth)*distance**2) * value
-            normalizer += np.exp(-(1/2*self.bandwidth)*distance**2)
+            weighted_sum += np.exp(-(distance**2/(2*self.bandwidth))) * value
+            normalizer += np.exp(-(distance**2/(2*self.bandwidth)))
         prediction = weighted_sum / normalizer
-        error = np.abs(query_point.iloc[-1] - prediction)
-        correct = "Correct" if error < self.error else "Incorrect"
+        squared_error = np.power(query_point.iloc[-1] - prediction, 2)
+        if not tuning and not testing:
+            self.update_training_stats(prediction, query_point.iloc[-1], error=squared_error, regression=True)
+        elif tuning:
+            self.update_tuning_stats(prediction, parameter, query_point.iloc[-1], error=squared_error, regression=True)
+        elif testing:
+            self.update_testing_stats(prediction, query_point.iloc[-1], error=squared_error, regression=True)
+        # correct = "Correct" if squared_error < self.error else "Incorrect"
         # print(f'{correct} -- Prediction: {prediction}, Actual: {query_point.iloc[-1]}')
-        print(f'{correct} -- Error: {error}')
+        # print(f'{correct} -- Error: {squared_error}')
         return prediction
 
-    def update_training_stats(self, prediction, real_class):
-        correct = prediction == real_class
+    def update_training_stats(self, prediction, real_class, error=None, regression=False):
+        if not regression:
+            correct = prediction == real_class
+        else:
+            correct = error < self.error
         if correct:
             self.training_stats['correct'] += 1
         else:
@@ -129,9 +148,14 @@ class KNN:
                     self.training_stats['false_negative'] += 1
         self.training_stats['total_predictions'] += 1
         self.training_stats['success_rate'] = self.training_stats['correct'] / self.training_stats['total_predictions']
+        if regression:
+            self.training_stats['mean_squared_error'] += error
 
-    def update_tuning_stats(self, prediction, parameter, real_class):
-        correct = prediction == real_class
+    def update_tuning_stats(self, prediction, parameter, real_class, error=None, regression=False):
+        if not regression:
+            correct = prediction == real_class
+        else:
+            correct = error < self.error
         if correct:
             self.parameter_tuning[parameter]['correct'] += 1
         else:
@@ -144,9 +168,14 @@ class KNN:
         self.parameter_tuning[parameter]['total_predictions'] += 1
         self.parameter_tuning[parameter]['success_rate'] = \
             self.parameter_tuning[parameter]['correct'] / self.parameter_tuning[parameter]['total_predictions']
+        if regression:
+            self.parameter_tuning[parameter]['mean_squared_error'] += error
 
-    def update_testing_stats(self, prediction, real_class):
-        correct = prediction == real_class
+    def update_testing_stats(self, prediction, real_class, error=None, regression=False):
+        if not regression:
+            correct = prediction == real_class
+        else:
+            correct = error < self.error
         if correct:
             self.test_stats['correct'] += 1
         else:
@@ -158,6 +187,8 @@ class KNN:
                     self.test_stats['false_negative'] += 1
         self.test_stats['total_predictions'] += 1
         self.test_stats['success_rate'] = self.test_stats['correct'] / self.test_stats['total_predictions']
+        if regression:
+            self.test_stats['mean_squared_error'] += error
 
     def report_training_stats(self):
         print("Training stats: \n", self.training_stats)
